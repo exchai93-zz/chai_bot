da50152f0ada058323fe8c61082519ff6ef1e859
Emily Chai <echai93@gmail.com>
2017-08-16 14:30:49 +0100

    Put keys into separate config.js file

---
+++ config.js
@@ -0,0 +1,7 @@
+// Object with keys 
+module.exports = {
+  consumer_key:         'XXk8DenQOz2WHCAne7h97noyA',
+  consumer_secret:      'opqTtcaurQxDljzakpHByKDzhW850GzLeaXtTaJpiSIgXBMDyY',
+  access_token:         '897804215128608768-0qSBHKu3so1CkqBUrxigmCoUUqsE3fD',
+  access_token_secret:  'uaiZTJxSAneJBInUUtbet4jlEYMOg4Knrejqn62c84ZF2',
+}
