ec90d84a91d29081d63e283693b15d25737a13bf
Emily Chai <echai93@gmail.com>
2017-08-16 15:12:36 +0100

    Playing around with scheduling tweets

--- chaibot.js
+++ chaibot.js
@@ -6,41 +6,64 @@ var config = require('./config');
 // Make new twit object with config info
 var T = new Twit(config);
 
-// GET REQUEST - searching on twitter
-// Object with parameters
-var params = {
-  q: 'holiday',
-  count: 5
-};
-// Get request
-T.get('search/tweets', params, gotData);
-// Callback function triggered when data returns from API
-function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i ++) {
-    console.log(tweets[i].text);
+
+function tweetIt() {
+
+  var r = Math.floor(Math.random()*100);
+  var tweet = {
+    status: 'Testing Chaibot: here is a random number ' + r + ' #pouringsomechai from node.js'
+  };
+
+  T.post('statuses/update', tweet, tweeted);
+  // Callback function
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Something went wrong!");
+    } else {
+      console.log("It worked!");
+    }
   }
+
 }
-// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
-  // console.log(data)
-// })
+
+tweetIt();
+setInterval(tweetIt, 1000*20);
 
 // POST REQUEST - tweeting
 //  Tweet object - one parameter
-var tweet = {
-  status: '#girlswhocode'
-};
-// Post to the Twitter API
-T.post('statuses/update', tweet, tweeted);
-// Callback function
-function tweeted(err, data, response) {
-  if (err) {
-    console.log("Something went wrong!");
-  } else {
-    console.log("It worked!");
-  }
-}
+// var tweet = {
+//   status: '#girlswhocode'
+// };
+// // Post to the Twitter API
+// T.post('statuses/update', tweet, tweeted);
+// // Callback function
+// function tweeted(err, data, response) {
+//   if (err) {
+//     console.log("Something went wrong!");
+//   } else {
+//     console.log("It worked!");
+//   }
+// }
 
 //
 // T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+// })
+
+// GET REQUEST - searching on twitter
+// Object with parameters
+// var params = {
+//   q: 'holiday',
+//   count: 5
+// };
+// // Get request
+// T.get('search/tweets', params, gotData);
+// // Callback function triggered when data returns from API
+// function gotData(err, data, response) {
+//   var tweets = data.statuses;
+//   for (var i = 0; i < tweets.length; i ++) {
+//     console.log(tweets[i].text);
+//   }
+// }
+// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+  // console.log(data)
 // })
